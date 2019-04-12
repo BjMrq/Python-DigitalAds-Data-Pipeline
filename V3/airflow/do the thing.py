445d738576fb8a3eb30518748f29b45bcafecf8d
BjMrq <44323490+BjMrq@users.noreply.github.com>
2019-04-12 08:07:35 -0400

    Add script to create scv

--- V3/airflow/do the thing.py
+++
@@ -1,9 +0,0 @@
-import fetching_facebook_data as fetch
-import cleanning_faceboo_data as clean
-import load_data_bigquery as load
-import delete_csv as delete
-
-fetch.main()
-clean.main()
-load.main()
-delete.main()
